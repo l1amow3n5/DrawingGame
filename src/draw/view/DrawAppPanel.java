d6edb44f3f9e5ba93346221432185ad72a8320f2
Liam Owen <l1amow3n5@yahoo.com>
2019-05-07 13:12:21 -0600

    added the 7 mouse action methods to the action listeners

--- src/draw/view/DrawAppPanel.java
+++ src/draw/view/DrawAppPanel.java
@@ -23,6 +23,7 @@ public class DrawAppPanel extends JPanel
 	private JButton loadButton;
 	private JButton saveButton;
 	private JButton clearButton;
+	private JButton randomColorButton;
 	
 	private JLabel widthLabel;
 	private JLabel imageLabel;
@@ -43,6 +44,7 @@ public class DrawAppPanel extends JPanel
 		super();
 		this.app = app;
 		canvas = new DrawPanel(app);
+		canvasPane = new JScrollPane();
 		
 		redButton = new JButton ("Red");
 		orangeButton = new JButton ("Orange");
@@ -51,6 +53,8 @@ public class DrawAppPanel extends JPanel
 		blueButton = new JButton ("Blue");
 		violetButton = new JButton ("Violet");
 		blackButton = new JButton ("Black");
+		randomColorButton = new JButton ("Random");
+		
 		loadButton = new JButton ("Load");
 		saveButton = new JButton ("Save");
 		clearButton = new JButton ("Clear");
@@ -69,6 +73,7 @@ public class DrawAppPanel extends JPanel
 		setupPanel();
 		setupLayout();
 		setupListeners();
+		setupSlider();
 	}
 	
 	private void setupPanel()
@@ -78,7 +83,29 @@ public class DrawAppPanel extends JPanel
 	
 	private void setupMenuPanels()
 	{
-		
+		colorPanel.setPreferredSize(new Dimension(50, 700));
+		menuPanel.setPreferredSize(new Dimension(50,700));
+				
+		violetButton.setForeground(new Color(75, 0, 130));
+		blueButton.setForeground(Color.BLUE);
+		greenButton.setForeground(Color.GREEN);
+		orangeButton.setForeground(Color.ORANGE);
+		redButton.setForeground(Color.RED);
+		yellowButton.setForeground(Color.YELLOW);
+				
+		colorPanel.add(blackButton);
+		colorPanel.add(violetButton);
+		colorPanel.add(blueButton);
+		colorPanel.add(greenButton);
+		colorPanel.add(yellowButton);
+		colorPanel.add(orangeButton);
+		colorPanel.add(redButton);
+		colorPanel.add(randomColorButton);
+				
+		menuPanel.add(widthSlider);
+		menuPanel.add(loadButton);
+		menuPanel.add(saveButton);
+		menuPanel.add(clearButton);
 	}
 	
 	private void setupScrollPane()
@@ -90,7 +117,33 @@ public class DrawAppPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
+		appLayout.putConstraint(SpringLayout.NORTH, colorPanel, 0, SpringLayout.NORTH, canvasPane);
+		appLayout.putConstraint(SpringLayout.SOUTH, colorPanel, 0, SpringLayout.SOUTH, canvasPane);
+		appLayout.putConstraint(SpringLayout.WEST, colorPanel, 50, SpringLayout.EAST, canvasPane);
+		appLayout.putConstraint(SpringLayout.EAST, colorPanel, 0, SpringLayout.WEST, menuPanel);
+				
+		appLayout.putConstraint(SpringLayout.WEST, menuPanel, 200, SpringLayout.EAST, canvasPane);
+		appLayout.putConstraint(SpringLayout.SOUTH, menuPanel, 0, SpringLayout.SOUTH, canvasPane);
+		appLayout.putConstraint(SpringLayout.EAST, menuPanel, -50, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.NORTH, menuPanel, 0, SpringLayout.NORTH, canvasPane);
+					
+		appLayout.putConstraint(SpringLayout.NORTH, canvasPane, 25, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, canvasPane, 50, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, canvasPane, -50, SpringLayout.SOUTH, this);
+	}
+	
+	private void setupSlider()
+	{
+		Hashtable<Integer, JLabel> scaleLabels = new Hashtable<Integer, JLabel>();
+		scaleLabels.put(MINIMUM_LINE, new JLabel("<HTML>Small<BR>Line</HTML>"));
+		scaleLabels.put(MAXIMUM_LINE, new JLabel("<HTML>Large<BR>Line</HTML>"));
+		widthSlider.setLabelTable(scaleLabels);
+		widthSlider.setSnapToTicks(true);
+		widthSlider.setMajorTickSpacing(5);
+		widthSlider.setMinorTickSpacing(1);
+		widthSlider.setPaintTicks(true);
+		widthSlider.setPaintLabels(true);
+		widthSlider.setValue((MAXIMUM_LINE + MINIMUM_LINE) / 2);
 	}
 	
 	private void setupListeners()
